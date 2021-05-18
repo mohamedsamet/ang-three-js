60f66b4b875558bf2e61452346bd0254e6bf9d4a
samsif <samet.development@gmail.com>
2021-05-18 10:54:28 +0100

    add box geo scene + animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,31 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = new THREE.WebGLRenderer();
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 1000 )
+const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(4, 12, 0x888888, 0x444444));
+const controle = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 5, 5);
+camera.lookAt(0, 0, 0);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(.4, .4, .4), new THREE.MeshBasicMaterial({color: 0x00ff00}));
+cube.position.set(.2, .2, .2)
+scene.add(cube);
+window.addEventListener('resize', onResize);
+
+export function animate(): any {
+  requestAnimationFrame(animate as any as FrameRequestCallback);
+  renderer.render(scene, camera);
+}
+
+export function onResize(): void {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 @Component({
   selector: 'app-root',
@@ -7,32 +33,16 @@ import * as THREE from 'three';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
-  public scene = new THREE.Scene();
-  public camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  public renderer = new THREE.WebGLRenderer();
   @ViewChild('scene') sceneDom: any;
 
   ngAfterViewInit(): void {
     this.initRenderer();
-    this.createCube();
   }
 
   initRenderer(): void {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.sceneDom.nativeElement.appendChild(this.renderer.domElement);
+    this.sceneDom.nativeElement.appendChild(renderer.domElement);
+    animate();
   }
 
-  createCube(): void {
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    this.camera.position.z = 5;
-    setInterval(() => {
-      cube.rotation.x += 0.1;
-      cube.rotation.y += 0.1;
-      this.scene.add( cube );
 
-      this.renderer.render(this.scene, this.camera);
-    }, 41);
-  }
 }
